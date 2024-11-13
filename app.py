df5c9643acbedb4349fe33365888af52e3b074f8
Xiaolan You <zyyou@liv>
2024-11-13 11:34:36 +0700

    Add Frozen Flask for static rendering

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
 
-@app.route('/recommendations')
+@app.route('/recommendations.html')
 def recommendations():
     return render_template('recommendations.html')
 
-@app.route('/books')
+@app.route('/books.html')
 def books():
     return render_template('books.html')
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.freeze()
