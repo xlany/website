49f83beae330cdc64731bcee56232a4d9f56f828
Xiaolan You <zyyou@liv>
2024-11-24 07:36:43 -0500

    Refactor JS

--- static/js/anchors.js
+++ static/js/anchors.js
@@ -1,22 +1,28 @@
 // static/js/anchors.js
-document.addEventListener("DOMContentLoaded", function () {
-    function updateDetailsFromHash() {
-        // Collapse all <details> elements
-        document.querySelectorAll("details").forEach((details) => {
-            details.open = false;
-        });
+/**
+ * Designed to trigger upon clicking an anchor link
+ *  i.e. http://xlany.com/recommendations.html#sunscreen
+ * Collapses all the existing opened sections
+ * Opens only the section specified in the anchor link
+ */
+function updateDetailsFromHash() {
+    // Collapse all <details> elements
+    document.querySelectorAll("details").forEach((details) => {
+        details.open = false;
+    });
 
-        // Expand the targeted <details> element if the hash matches
-        const hash = window.location.hash;
-        if (hash) {
-            const targetDetails = document.querySelector(hash);
-            if (targetDetails && targetDetails.tagName === "DETAILS") {
-                targetDetails.open = true; // Expand the targeted <details>
-                targetDetails.scrollIntoView({ behavior: "smooth", block: "start" });
-            }
+    // Expand the targeted <details> element if the hash matches
+    const hash = window.location.hash;
+    if (hash) {
+        const targetDetails = document.querySelector(hash);
+        if (targetDetails && targetDetails.tagName === "DETAILS") {
+            targetDetails.open = true; // Expand the targeted <details>
+            targetDetails.scrollIntoView({ behavior: "smooth", block: "start" });
         }
     }
+}
 
+document.addEventListener("DOMContentLoaded", function () {
     // Update <details> when the page is loaded with a hash
     updateDetailsFromHash();
 
